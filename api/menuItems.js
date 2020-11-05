cb9434feec80dd3c745ac603ce98381768517f07
browe83 <rowe.brandon.j@gmail.com>
2020-11-05 14:20:38 -0500

    wires up menuItems get route

--- api/menuItems.js
+++ api/menuItems.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 
-const menuItemsRouter = express.Router();
+const menuItemsRouter = express.Router({ mergeParams: true });
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 menuItemsRouter
   .get('/', (req, res, next) => {
-    res.send('received menuItems get req');
+    db.all(`SELECT * FROM MenuItem WHERE menu_id = ${req.params.menuId};`,
+      (err, menuItems) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(200).json({ menuItems });
+        }
+      });
   });
 
 module.exports = menuItemsRouter;
