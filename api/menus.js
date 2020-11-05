8316195084bd2070f6e883655d5e3045abb1e4cb
browe83 <rowe.brandon.j@gmail.com>
2020-11-05 14:01:46 -0500

    wires up menus delete route

--- api/menus.js
+++ api/menus.js
@@ -39,6 +39,49 @@ menusRouter
         }
       });
   })
+  .put('/:menuId', (req, res, next) => {
+    const { title } = req.body.menu;
+    if (!title) {
+      return res.sendStatus(400);
+    }
+    db.run(`UPDATE Menu SET title = $title WHERE id = ${req.menu.id}`,
+      {
+        $title: title,
+      },
+      (err) => {
+        if (err) {
+          next(err);
+        } else {
+          db.get(`SELECT * FROM Menu WHERE id = ${req.menu.id};`,
+            (err, menu) => {
+              if (err) {
+                next(err);
+              } else {
+                res.status(200).send({ menu });
+              }
+            });
+        }
+      });
+  })
+  .delete('/:menuId', (req, res, next) => {
+    db.all(`SELECT * FROM MenuItem WHERE menu_id = ${req.menu.id};`,
+      (err, menuItems) => {
+        if (err) {
+          next(err);
+        } else if (menuItems.length === 0) {
+          db.run(`DELETE FROM Menu WHERE id = ${req.menu.id};`, 
+            (err) => {
+              if (err) {
+                next(err);
+              } else {
+                res.sendStatus(204);
+              }
+            });
+        } else {
+          res.sendStatus(400);
+        }
+      });
+  })
   .post('/', (req, res, next) => {
     const { title } = req.body.menu;
     if (!title) {
