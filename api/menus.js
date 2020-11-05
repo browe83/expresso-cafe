14a162a8ba247d5be15ea6b18bb8b94dc74c328e
browe83 <rowe.brandon.j@gmail.com>
2020-11-05 11:54:12 -0500

    wires up menuId get route

--- api/menus.js
+++ api/menus.js
@@ -6,6 +6,19 @@ const menusRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 menusRouter
+  .param('menuId', (req, res, next, id) => {
+    db.get(`SELECT * FROM Menu WHERE id = ${id};`,
+      (err, menu) => {
+        if (err) {
+          next(err);
+        } else if (menu) {
+          req.menu = menu;
+          next();
+        } else {
+          res.sendStatus(404);
+        }
+      });
+  })
   .get('/', (req, res, next) => {
     db.all('SELECT * FROM Menu;',
       (err, menus) => {
@@ -13,6 +26,16 @@ menusRouter
           next(err);
         } else {
           res.status(200).send({ menus });
+        }
+      });
+  })
+  .get('/:menuId', (req, res, next) => {
+    db.get(`SELECT * FROM Menu WHERE id = ${req.menu.id}`,
+      (err, menu) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(200).json({ menu });
         }
       });
   })
