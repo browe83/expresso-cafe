3065dcf6c157417d3993dd27378a6470c2b4b3bc
browe83 <rowe.brandon.j@gmail.com>
2020-11-05 11:25:15 -0500

    wires up post route for menus

--- api/menus.js
+++ api/menus.js
@@ -15,6 +15,29 @@ menusRouter
           res.status(200).send({ menus });
         }
       });
+  })
+  .post('/', (req, res, next) => {
+    const { title } = req.body.menu;
+    if (!title) {
+      return res.sendStatus(400);
+    }
+    db.run('INSERT INTO Menu (title) VALUES ($title)', 
+      {
+        $title: title,
+      },
+      function(err) {
+        if (err) {
+          next(err);
+        }
+        db.get(`SELECT * FROM Menu WHERE id = ${this.lastID};`,
+          (err, menu) => {
+            if (err) {
+              next(err);
+            } else {
+              res.status(201).send({ menu });
+            }
+          });
+      });
   });
 
 module.exports = menusRouter;
